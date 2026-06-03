f60253e9cd8a7442ecfce13834193c4446ee0a06
Dmitriy Romanov <d.romanov@goodt.me>
2026-06-03 22:01:48 +0500

    fixed global types definition

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,28 @@
 /// <reference types="node" />
 
+// Для Node.js проектов (если есть @types/node)
+declare module "node:console" {
+	interface Console {
+		group(label: string): string;
+		groupCollapsed(label: string): string;
+		groupEnd(id?: string);
+	}
+}
+
+declare module "console" {
+	interface Console {
+		group(label: string): string;
+		groupCollapsed(label: string): string;
+		groupEnd(id?: string);
+	}
+}
+
 declare global {
 	interface Console {
 		group(label: string): string;
 		groupCollapsed(label: string): string;
+		groupEnd(id?: string);
 	}
 }
+
 export {};
